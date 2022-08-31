75bbed7b6f062776956f0cfc75b1e321c1f8fae0
Jerry Smidt <jerry@parse.nl>
2022-08-31 15:22:21 +0200

    Feature #55583: Improve autocomplete field validation
    * Mark field invalid and show inline message until an address is
      selected.

--- postcodenl-address-autocomplete.php
+++ postcodenl-address-autocomplete.php
@@ -3,7 +3,7 @@
  * Plugin Name: Postcode.nl Address Autocomplete
  * Plugin URI: https://www.postcode.nl/en/services/adresdata/implementatie
  * Description: Reference implementation for Postcode.nl international address autocomplete for WooCommerce
- * Version: 2.0.5
+ * Version: 2.0.6
  * Author: Postcode.nl
  * Author URI: https://www.postcode.nl
  * Text Domain: postcodenl-address-autocomplete

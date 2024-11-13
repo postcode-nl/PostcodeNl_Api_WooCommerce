554457f6ec6ca92a283d66cc4bba9cf75f21a4f0
Jerry Smidt <jerry@parse.nl>
2024-11-13 11:19:08 +0100

    Feature #58588: Add build files for production

---
+++ build/billing-address-autocomplete-intl-index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks'), 'version' => 'f901108df2f066170382');
---
+++ build/shipping-address-autocomplete-intl-index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks'), 'version' => '914d16d6b20e3cc70094');
